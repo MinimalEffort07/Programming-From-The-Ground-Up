de6e6295f8bc3eae37ebb5fa75266b17cbde9cc9
MinimalEffort07 <90430937+MinimalEffort07@users.noreply.github.com>
2023-08-20 19:48:08 +1000

    Added comments

--- exit/exit-return-3.s
+++ exit/exit-return-3.s
@@ -1,3 +1,19 @@
+# PURPOSE
+#   Return status code '3' to the kernel. 
+#
+# AUTHOR
+#   Emmanuel Christianos
+#
+# INPUT
+#   none
+#
+# OUTPUT
+#   Returns status code '3' - Observed by $?
+#
+# VARIABLES 
+#   %eax holds syscall number for SYS_EXIT
+#   %ebx holds status code value to be returned
+
 .section .data
 .section .text
 .globl _start
