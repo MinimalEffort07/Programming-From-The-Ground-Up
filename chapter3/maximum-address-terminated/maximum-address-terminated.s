1e83048b417feee9c46dce8362dc07aee2760731
MinimalEffort07 <90430937+MinimalEffort07@users.noreply.github.com>
2023-08-20 19:48:08 +1000

    update

--- chapter3/maximum-address-terminated/maximum-address-terminated.s
+++ chapter3/maximum-address-terminated/maximum-address-terminated.s
@@ -6,53 +6,61 @@
 #   Emmanuel Christianos
 #
 # Data:
-#   data_items: array of 4 byte integers  
-#   length_data_items: number of numbers in data_items  
+#   items: array of 4 byte integers  
+#   len_items: number of numbers in items  
 #
 # Variables:
-#   %eax = address at the current index into the array of numbers (data_items)
+#   %eax = address at the current index into the array of numbers (items)
 #   %ebx = Current maximum value
-#   %ecx = Address 1 after the last item of data_items
+#   %ecx = Address 1 after the last item of items
 #   %edx = Length of data items (counting from 0)
-#   %edi = Current index value into data_items
-#   %esi = scratch register used for holding temp value in calculating end address
+#   %edi = Current index value into items
 
 .globl _start
 
 .section .data
 
-length_data_items:
-.long 9
+len_items:
+.long 0
 
-data_items:
+items:
 .long 1,32,42,2,4,200,2,0,253
 
 .section .text
 
 _start: 
-    mov length_data_items, %edx
-    decl %edx
-    mov $4, %esi
-    leal data_items(%esi, %edx, 4), %ecx    # Load the address after the last item
-                                            # in data_items into %ecx
-    movl $0, %edi                           # Initialize the index variable 
-    leal data_items(, %edi, 4), %eax        # Initializes %eax to store the address first value of data_items
-    movl (%eax), %ebx                       # Initializes %eax to store first item as current max
+    movl len_items, %edx            # Store value at len_items in edx
+    movl $1, %ebx                   # Set return value in case of early exit
+    cmpl $0, %edx                   # Compare len_items against 0
+    je exit                         # Jump to exit if len_items == 0
+    leal items(, %edx, 4), %ecx     # Load the address after the last item in 
+                                    # items into %ecx i.e. &items[len_items*4]
+
+    movl $0, %edi                   # Initialize items index to 0
+    leal items(, %edi, 4), %eax     # Store the address of items[index*4] into 
+                                    # eax
+    movl (%eax), %ebx               # Store the value at eax in ebx
 
 start_loop:
-        
-    cmpl %ecx, %eax                         # Compare current value against 255
-    je exit                                 # Jump to exit symbol if current value is 255
+    cmpl %ecx, %eax                 # Compare address of items[index*4] i.e. eax
+                                    # against terminating address stored in ecx
 
-    incl %edi                               # Increment the counter
-    leal data_items(, %edi, 4), %eax        # Store data_list+edi in eax
+    je exit                         # Jump to exit symbol if address of 
+                                    # items[index*4] and terminating address are 
+                                    # equal. 
 
-    cmpl %ebx, (%eax)                       # Compare current value against maximum 
-    jle start_loop                          # Jump back to start of loop if current value is <= max
+    incl %edi                       # Increment index value
+    leal items(, %edi, 4), %eax     # Store address of items[index*4] in eax
 
-    movl (%eax), %ebx                       # Current val is greater than max, update max to current val
-    jmp start_loop                          # Jump to start of loop
+    cmpl %ebx, (%eax)               # Compare current value i.e items[index*4] 
+                                    # against maximum
+    jle start_loop                  # Jump back to start of loop if current 
+                                    # value is <= max
+
+    movl (%eax), %ebx               # Current value is greater than max, update 
+                                    # max to current val
+    jmp start_loop                  # Jump to start of loop
 
 exit:
-    movl $1, %eax                           # Move value 1 syscall (exit) into eax
-    int $0x80                               # Syscall interrupt
+    movl $1, %eax                   # Move value 1 syscall (exit) into eax
+    int $0x80                       # Syscall interrupt
--- chapter3/maximum-count-terminiated/maximum-address-terminated.s
+++ chapter3/maximum-count-terminiated/maximum-address-terminated.s
@@ -1,58 +1,55 @@
 # Purpose: 
-#   To find the maximum value of a given array of numbers, using a calculated 
-#   address to determine when to stop reading from the data list. 
+#   To find the maximum value of a given array of numbers, using a count to  
+#   determine when to stop reading from the data list. 
 #
 # Author:
 #   Emmanuel Christianos
 #
 # Data:
-#   data_items: array of 4 byte integers  
-#   length_data_items: number of numbers in data_items  
+#   items: array of 4 byte integers  
+#   len_items: number of numbers in items  
 #
 # Variables:
-#   %eax = address at the current index into the array of numbers (data_items)
+#   %eax = address at the current index into the array of numbers (items)
 #   %ebx = Current maximum value
-#   %ecx = Address 1 after the last item of data_items
-#   %edx = Length of data items (counting from 0)
-#   %edi = Current index value into data_items
-#   %esi = scratch register used for holding temp value in calculating end address
+#   %ecx = Address 1 after the last item of items
+#   %edx = Length of data items
+#   %edi = Current index value into items
 
 .globl _start
 
 .section .data
 
-length_data_items:
+len_items:
 .long 9
 
-data_items:
+items:
 .long 1,32,42,2,4,200,2,0,253
 
 .section .text
 
 _start: 
-    mov length_data_items, %edx
-    decl %edx
-    mov $4, %esi
-    leal data_items(%esi, %edx, 4), %ecx    # Load the address after the last item
-                                            # in data_items into %ecx
-    movl $0, %edi                           # Initialize the index variable 
-    leal data_items(, %edi, 4), %eax        # Initializes %eax to store the address first value of data_items
-    movl (%eax), %ebx                       # Initializes %eax to store first item as current max
+    movl len_items, %edx            # Store length of list in edx
+    movl $0, %edi                   # Initialize the index variable 
+    movl $1, %ebx                   # Initialise ebx in case of early exit
+    cmpl %edx, %edi                 # Compare 0 with len_items
+    je exit                         # Jump to exit if length of items is 0
+    movl items(, %edi, 4), %eax     # Initializes %eax to store the address first value of items
+    movl %eax, %ebx                 # Initializes %eax to store first item as current max
 
 start_loop:
-        
-    cmpl %ecx, %eax                         # Compare current value against 255
-    je exit                                 # Jump to exit symbol if current value is 255
+    cmpl %edx, %edi                 # Compare index value against length of list 
+    je exit                         # Jump to exit symbol if index == len_items
 
-    incl %edi                               # Increment the counter
-    leal data_items(, %edi, 4), %eax        # Store data_list+edi in eax
+    incl %edi                       # Increment the counter
+    movl items(, %edi, 4), %eax     # Store data_list+edi in eax
 
-    cmpl %ebx, (%eax)                       # Compare current value against maximum 
-    jle start_loop                          # Jump back to start of loop if current value is <= max
+    cmpl %ebx, %eax                 # Compare current value against maximum 
+    jle start_loop                  # Jump back to start of loop if current value is <= max
 
-    movl (%eax), %ebx                       # Current val is greater than max, update max to current val
-    jmp start_loop                          # Jump to start of loop
+    movl %eax, %ebx                 # Current val is greater than max, update max to current val
+    jmp start_loop                  # Jump to start of loop
 
 exit:
-    movl $1, %eax                           # Move value 1 syscall (exit) into eax
-    int $0x80                               # Syscall interrupt
+    movl $1, %eax                   # Move value 1 syscall (exit) into eax
+    int $0x80                       # Syscall interrupt
